2295e342c3bc35d10ee63edea2d25999187c0543
Reyad <jubaidul.ctg.bd@gmail.com>
2020-11-09 16:54:49 +0600

    update login url

--- config/config.ts
+++ config/config.ts
@@ -69,6 +69,32 @@ export default defineConfig({
               path: '/',
               redirect: '/user/login',
             },
+
+            {
+              path: '/dashboard',
+              name: 'dashboard',
+              icon: 'dashboard',
+              routes: [
+                // {
+                //   name: 'analysis',
+                //   icon: 'smile',
+                //   path: '/dashboard/analysis',
+                //   component: './dashboard/analysis',
+                // },
+                {
+                  name: 'monitor',
+                  icon: 'smile',
+                  path: '/dashboard/monitor',
+                  component: './dashboard/monitor',
+                },
+                // {
+                //   name: 'workplace',
+                //   icon: 'smile',
+                //   path: '/dashboard/workplace',
+                //   component: './dashboard/workplace',
+                // },
+              ],
+            },
             {
               path: '/category',
               icon: 'form',
@@ -125,31 +151,7 @@ export default defineConfig({
 
             
           
-          {
-            path: '/dashboard',
-            name: 'dashboard',
-            icon: 'dashboard',
-            routes: [
-              // {
-              //   name: 'analysis',
-              //   icon: 'smile',
-              //   path: '/dashboard/analysis',
-              //   component: './dashboard/analysis',
-              // },
-              {
-                name: 'monitor',
-                icon: 'smile',
-                path: '/dashboard/monitor',
-                component: './dashboard/monitor',
-              },
-              // {
-              //   name: 'workplace',
-              //   icon: 'smile',
-              //   path: '/dashboard/workplace',
-              //   component: './dashboard/workplace',
-              // },
-            ],
-          },
+          
           //   {
           //     path: '/form',
           //     icon: 'form',
